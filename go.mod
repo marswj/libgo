e45ec3f8416ddcfbbeb2c5df0692c62b8ca35d64
wj <marswj@qq.com>
2019-11-06 20:08:58 +0800

    1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module libgo
+
+go 1.13
+
+require github.com/gofrs/uuid v3.2.0+incompatible
--- time.go
+++ time.go
@@ -13,6 +13,21 @@ func TimeStr2(t int64) string {
 	return t1.Format("2006-01-02 15:04:05")
 }
 
+//当前时间的全格式
+func TimeStrNow() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+//
+func TimeStrYMD(t time.Time) string {
+	return t.Format("2006-01-02")
+}
+
+//当前时间ymd格式
+func TimeStrYmdNow(t time.Time) string {
+	return time.Now().Format("2006-01-02")
+}
+
 //unix时间戳
 func TimeUnix() int64 {
 	return time.Now().Unix()
